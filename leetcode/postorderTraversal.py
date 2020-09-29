12090892af50b3cc1d56eeb10631b4c9b5c49ef1
leiyi <2457738122@qq.com>
2020-09-29 23:45:46 +0800

    145. 二叉树的后序遍历

--- leetcode/postorderTraversal.py
+++ leetcode/postorderTraversal.py
@@ -18,6 +18,7 @@
 官方解法，还在研究ing，只知道递归。。。
 """
 
+
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         if not root:
@@ -41,9 +42,3 @@ class Solution:
                 root = root.right
 
         return res
-
-
-作者：LeetCode - Solution
-链接：https: // leetcode - cn.com / problems / binary - tree - postorder - traversal / solution / er - cha - shu - de - hou - xu - bian - li - by - leetcode - solution /
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
