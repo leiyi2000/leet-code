e6567f8f62c935651140e1ce0fd57fdeaa2597bd
leiyi <2457738122@qq.com>
2020-09-30 13:41:25 +0800

    701. 二叉搜索树中的插入操作

---
+++ leetcode/insertIntoBST.py
@@ -0,0 +1,67 @@
+"""
+701. 二叉搜索树中的插入操作
+给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据保证，新值和原始二叉搜索树中的任意节点值都不同。
+
+注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。
+
+
+
+例如,
+
+给定二叉搜索树:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+和 插入的值: 5
+你可以返回这个二叉搜索树:
+
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+或者这个树也是有效的:
+
+         5
+       /   \
+      2     7
+     / \
+    1   3
+         \
+          4
+"""
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+
+        if root is None:
+            return
+
+        rc = root
+
+        if root.val < val:
+            if self.insertIntoBST(root.right, val):
+                root.right = TreeNode(val)
+
+        else:
+            if self.insertIntoBST(root.left, val):
+                root.left = TreeNode(val)
+
+        return rc
